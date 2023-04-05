d72e8fb061139f216d24a15677d3168e88d38513
Egor <egor_chek@mail.ru>
2023-04-05 23:01:10 +0400

    Initial Commit

---
+++ SomeChat/SomeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeChatApp.swift
+//  SomeChat
+//
+//  Created by admin on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SomeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
